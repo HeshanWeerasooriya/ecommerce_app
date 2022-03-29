a677c479b601af7dcdc7135f13deb0f62b042edc
Heshan <heshan.imedha@gmail.com>
2022-03-29 15:00:18 +0530

    Carousel Sliders

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,7 +1,9 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:ecommerce_app/models/category_model.dart';
 import 'package:flutter/material.dart';
-
 import '../../widgets/custom_appbar.dart';
 import '../../widgets/custom_navbar.dart';
+import '../../widgets/hero_caraosel_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -17,8 +19,19 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: 'Awesome Deals'),
+    return Scaffold(
+      appBar: CustomAppBar(title: 'YouBay'),
+      body: CarouselSlider(
+        options: CarouselOptions(
+          aspectRatio: 1.5,
+          viewportFraction: 0.9,
+          enlargeCenterPage: true,
+          enlargeStrategy: CenterPageEnlargeStrategy.height,
+        ),
+        items: Category.categories
+            .map((Category) => HeroCarouselCard(category: Category))
+            .toList(),
+      ),
       bottomNavigationBar: CustomNavBar(),
     );
   }
